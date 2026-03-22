5e50a8785be7cf72119e442bfc31ae49bd87d1dc
ch-land <cmf@chrysanthemum.us>
2026-03-22 15:39:14 -0500

    Initial Commit

---
+++ ExifArmor/ExifArmorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExifArmorApp.swift
+//  ExifArmor
+//
+//  Created by Christian Flores on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct ExifArmorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
